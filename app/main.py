b68e6dd552a71d3e5925cfd7f51e6dddf7a11229
qkitzero <qkitzero@gmail.com>
2025-05-06 18:31:25 +0900

    add: health

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
-from app.interface import quiz_handler
+from app.interface import health_handler, quiz_handler
 
 app = FastAPI()
+app.include_router(health_handler.router)
 app.include_router(quiz_handler.router)
